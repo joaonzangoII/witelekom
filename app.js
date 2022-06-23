cdd41f7e02c6e9a976bbcf2fdf251681a45c5c73
May Gomes <35641653+mayragss@users.noreply.github.com>
2022-06-23 05:39:45 -0300

    fix port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express'),
       app =  express(),
-      port = 3200,
+      port = process.env.PORT || 3000,
       bodyParser = require('body-parser'),
       path = require('path'),
       methodOverride = require('method-override'),
