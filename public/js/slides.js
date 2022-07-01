718b8b7cbc368575e5273ca86df9edbf9cc7f567
May Gomes <35641653+mayragss@users.noreply.github.com>
2022-06-30 23:42:53 -0300

    add changes

--- public/js/slides.js
+++ public/js/slides.js
@@ -1,17 +1,19 @@
 
 let slideIndex = 0;
-showSlides();
+//showSlides();
 
 function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
+
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].className = "mySlides fade";
     }
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 8000); // Change image every 2 seconds
+    slides[slideIndex - 1].className = "mySlides active fade";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
 function showChoosedSlides(n) {
@@ -39,4 +41,20 @@ function hideMenu(){
 }
 function showMenu(){
     document.getElementById("menu").style.display = 'block';
+}
+
+function openLanguageMenu() {
+    if( document.getElementById("language-menu").style.display == 'block')
+        document.getElementById("language-menu").style.display = 'none';
+    else
+        document.getElementById("language-menu").style.display = 'block';
+}
+
+function changeLanguage(current, location) {
+    if(current == 'PT'){
+        window.location.replace(`${location}pt`);
+    }
+    else{
+        window.location.replace(`${location}en`);
+    }
 }
